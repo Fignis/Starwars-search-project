b05c720e27f7a43946a1cd8e8225253471b99aa7
Fignis <35942635+Fignis@users.noreply.github.com>
2020-12-24 00:01:15 -0500

    Add files via upload

--- starwars-search-project/app.js
+++ starwars-search-project/app.js
@@ -30,32 +30,6 @@ async function req() {
   //call the display function.
   display(fData);
 }
-// async function starReq() {
-//   //get userinput value
-//   let usrinput = document.querySelector("input").value;
-//   //get the data from the api based on what the user searched for
-//   const { data } = await axios.get(apiStarShip + usrinput);
-//   //destructure the object
-//   const {
-//     results: [fData],
-//   } = data;
-//   console.log(fData);
-//   //call the display function.
-//   display(fData);
-// }
-// async function planetReq() {
-//   //get userinput value
-//   let usrinput = document.querySelector("input").value;
-//   //get the data from the api based on what the user searched for
-//   const { data } = await axios.get(apiPlanets + usrinput);
-//   //destructure the object
-//   const {
-//     results: [fData],
-//   } = data;
-//   console.log(fData);
-//   //call the display function.
-//   display(fData);
-// }
 
 //displays the data
 function display(data) {
@@ -78,68 +52,3 @@ input.addEventListener("keyup", function (e) {
 });
 
 button.addEventListener("click", req);
-// stButton.addEventListener("click", starReq);
-// plButton.addEventListener("click", planetReq);
-
-// // console.log("start");
-
-// // function loginUser(email, password, onSuccess, onFail) {
-// //   setTimeout(() => {
-// //     console.log("data is here");
-// //     onSuccess({ usrEmail: email });
-// //   }, 2000);
-// // }
-// // function getVideos(email, callback) {
-// //   setTimeout(() => {
-// //     callback(["vid1", "vid2", "vid3"]);
-// //   }, 1000);
-// // }
-// // const user = loginUser("john@gmail.com", 1234, (user) => {
-// //   console.log(user);
-// //   getVideos(user.usrEmail, (videos) => {
-// //     console.log(videos);
-// //   });
-// // });
-// // console.log(user);
-// // console.log("finished");
-
-// function loginUser(email, password) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("data is here");
-//       resolve({ usrEmail: email });
-//     }, 2000);
-//   });
-// }
-
-// function getVideos(email) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(["vid1", "vid2", "vid3"]);
-//     }, 1000);
-//   });
-// }
-// loginUser("john", 12345).then((user) => getVideos(user.email));
-
-// //execute code atthe sametime
-// /*const yt = new Promise((resolve) => {
-//   setTimeout(() => {
-//     console.log("getting stuff from yt");
-//     resolve({ videos: [1, 2, 3, 4] });
-//   }, 2000);
-// });
-
-// const fb = new Promise((resolve) => {
-//   setTimeout(() => {
-//     console.log("getting stuff from fb");
-//     resolve({ user: "name" });
-//   }, 2000);
-// });
-// Promise.all([yt, fb]).then((result) => {
-//   console.log(result);
-// });
-// */
-// async function displayUsr() {
-//   const loginuser = await loginUser("john", 12345);
-//   const videos = await getVideos(loginuser.email);
-// }
