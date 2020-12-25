60a9ff4e2656e9e82589c924eaec1bb4886b4ff4
Fignis <35942635+Fignis@users.noreply.github.com>
2020-12-25 15:22:58 -0500

    Made updates to styles and added animations

--- starwars-search-project/app.js
+++ starwars-search-project/app.js
@@ -1,13 +1,14 @@
 let button = document.querySelector("#button");
-let plButton = document.querySelector("#button2");
-let stButton = document.querySelector("#button3");
+let redirButton = document.getElementById("redirbutt");
 let api = "https://swapi.dev/api/people/?search=";
+let logo = document.querySelector(".logo");
 const apiStarShip = "https://swapi.dev/api/starships/?search=";
 const apiPlanets = "https://swapi.dev/api/planets/?search=";
 let dat = document.querySelector(".data");
 let input = document.querySelector("input");
 let html = window.location.href;
 var file = html.substring(html.lastIndexOf("/") + 1);
+const resultCon = document.getElementById("res");
 
 //function to get the data and destructure the data
 console.log(file);
@@ -19,6 +20,11 @@ if (file == "starship.html") {
 async function req() {
   //get userinput value
   let usrinput = document.querySelector("input").value;
+  if (usrinput === "") {
+    usrinput = undefined;
+  } else {
+    usrinput = document.querySelector("input").value;
+  }
   //get the data from the api based on what the user searched for
   const { data } = await axios.get(api + usrinput);
 
@@ -29,10 +35,15 @@ async function req() {
   console.log(fData);
   //call the display function.
   display(fData);
+  resultCon.classList.add("rescontainer");
+  smooth(dat);
 }
-
+function smooth(button) {
+  button.scrollIntoView({ block: "center", behavior: "smooth" });
+}
 //displays the data
 function display(data) {
+  dat.innerHTML = "";
   //transform the recieved data from an object to an associative array of rows and entries
   const entries = Object.entries(data);
   //destructure the array into something that is iterable for the "for of loop";
@@ -50,5 +61,11 @@ input.addEventListener("keyup", function (e) {
     button.click();
   }
 });
+const redir = () => {
+  smooth(logo);
+  dat.innerText = "";
+  resultCon.classList.toggle("rescontainer");
+};
 
 button.addEventListener("click", req);
+redirButton.addEventListener("click", redir);
